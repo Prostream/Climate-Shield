12d970ce1fb5f0ac86d83bda915dfde5e3345602
JeffTanChina <jeff.tandev@gmail.com>
2024-10-28 17:09:14 -0700

    登录

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// 随机生成一些用户数据存放在数组中
+const users = [
+  { username: '1', password: '1' },
+  { username: '999', password: '1234' },
+  { username: 'HaoyangTan', password: '2233' },
+];
+
 // 中间件配置
 app.use(cors());
 app.use(express.json());
@@ -11,6 +18,22 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
   });
+
+// 登录接口
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // 查找匹配的用户
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (user) {
+    res.status(200).json({ message: 'Login successful', user: { username: user.username } });
+  } else {
+    res.status(401).json({ message: 'Invalid username or password' });
+  }
+});
 
 // 设置服务器端口
 const PORT = process.env.PORT || 5000;
